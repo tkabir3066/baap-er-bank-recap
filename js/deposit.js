e21954c908c139c1f2c511127164d36daff3d045
Tutul Kabir <tutulkabir@gmail.com>
2023-08-23 11:53:30 +0530

    Update deposit and balance

--- js/deposit.js
+++ js/deposit.js
@@ -6,6 +6,8 @@
 3. clear the deposit input field after getting the value
 4. get the previous deposit total
 5. calculate the new deposit total and set the value to the deposit total
+6.get current balance total
+7.calculate the new balance total and set it to the balance total element
 */
 
 //step-1
@@ -35,4 +37,14 @@ document.getElementById("btn-deposit").addEventListener("click", () => {
   //  step-5
   const newDepositTotal = previousDepositTotalAmount + newDepositAmount;
   depositTotalElement.innerText = newDepositTotal;
+
+  //step-6:
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  console.log(previousBalanceTotal);
+
+  // step-7:
+  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  balanceTotalElement.innerText = newBalanceTotal;
 });
